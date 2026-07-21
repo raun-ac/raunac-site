22e7219d671d889c1f54f833816ed07a6b1f0f9b
raunac <raunakchaudhary787@gmail.com>
2026-07-21 19:05:29 +0530

    minor changes

--- src/consts.ts
+++ src/consts.ts
@@ -1,11 +1,11 @@
 import type { Site, Metadata, Socials } from "@types";
 
 export const SITE: Site = {
-  NAME: "home",
+  NAME: "raunac",
   EMAIL: "raun.ac27@gmail.com",
-  NUM_POSTS_ON_HOMEPAGE: 3,
-  NUM_WORKS_ON_HOMEPAGE: 2,
-  NUM_PROJECTS_ON_HOMEPAGE: 3,
+  NUM_POSTS_ON_HOMEPAGE: 0,
+  NUM_WORKS_ON_HOMEPAGE: 0,
+  NUM_PROJECTS_ON_HOMEPAGE: 0,
 };
 
 export const HOME: Metadata = {
