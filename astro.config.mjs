bf198f8475068839d96aeb508fded31a38de9b1d
raunac <raunakchaudhary787@gmail.com>
2026-08-19 15:27:37 +0530

    configured astro.config.mjs for custom domain and also added public/CNAME

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,12 +6,12 @@ import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
 
 export default defineConfig({
-  site: "https://astro-nano-demo.vercel.app",
+  site: "https://raunac.site",
 
   markdown: {
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeKatex],
   },
-  
+
   integrations: [mdx(), sitemap(), tailwind()],
 });
